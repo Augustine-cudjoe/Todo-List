d91633519a4550302c2b2d7fd287d3487121244b
Augustine Cudjoe <augustinecudjoe@Augustines-MacBook-Pro.local>
2023-06-27 22:53:55 +0200

    todo list

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 
 var add=document.getElementById("addToDo");
+var input =document.getElementById("inputField");
+var todoContainer=document.getElementById("todoContainer");
+add.addEventListener('click', addItem);
+input.addEventListener('keypress',function(e){
+    addItem();
+})
+
+function addItem(e){
+
+    const item_value =input.value;
+    const item = document.createElement("div");
+    item.classList.add("item");
+
+    const item_content = document.createElement("div");
+    item_content.classList.add("content");
+    item.appendChild(" item_content");
+
+}
